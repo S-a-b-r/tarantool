453950ffef1db96923fe65c9b086e519ba0970b6
g.shamshurin <g.shamshurin@bars-arenda.ru>
2024-10-08 14:53:37 +0400

    fix errors and open instance ports

--- main.go
+++ main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/tarantool/go-tarantool/v2"
-	"github.com/tarantool/go-tarantool/v2/pool"
 	"time"
+
+	"github.com/tarantool/go-tarantool/v2"
 )
 
 func main() {
 	// Connect to the database
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-
 	dialer := tarantool.NetDialer{
-		Address:  "127.0.0.1:3302",
+		Address:  "localhost:3302",
 		User:     "sampleuser",
 		Password: "123456",
 	}
@@ -22,25 +21,45 @@ func main() {
 		Timeout: time.Second,
 	}
 
-	instances := []pool.Instance{{"instance-001", dialer, opts}}
-
-	conn, err := pool.Connect(ctx, instances)
-
-	//conn, err := tarantool.Connect(ctx, dialer, opts)
+	conn, err := tarantool.Connect(ctx, dialer, opts)
 	if err != nil {
 		fmt.Println("Connection refused:", err)
 		return
 	}
 
 	// Select by primary key
-	data, err := conn.DoInstance(
+	data, err := conn.Do(
 		tarantool.NewSelectRequest("bands").
-			Limit(1).
-			Key([]interface{}{uint(1)}), "instance-001",
+			Limit(10).
+			Iterator(tarantool.IterEq).
+			Key([]interface{}{uint(1)}),
 	).Get()
-
 	if err != nil {
 		fmt.Println("Got an error:", err)
 	}
 	fmt.Println("Tuple selected by the primary key value:", data)
+
+	// Insert data
+	// tuples := [][]interface{}{
+	// 	{1, "Roxette", 1986},
+	// 	{2, "Scorpions", 1965},
+	// 	{3, "Ace of Base", 1987},
+	// 	{4, "The Beatles", 1960},
+	// }
+	// var futures []*tarantool.Future
+	// for _, tuple := range tuples {
+	// 	request := tarantool.NewInsertRequest("bands").Tuple(tuple)
+	// 	futures = append(futures, conn.Do(request))
+	// }
+	//
+	// fmt.Println("Inserted tuples:")
+	// for _, future := range futures {
+	// 	result, err := future.Get()
+	// 	if err != nil {
+	// 		fmt.Println("Got an error:", err)
+	// 	} else {
+	// 		fmt.Println(result)
+	// 	}
+	// }
+
 }
